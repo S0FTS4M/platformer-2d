5fcd3c78d4b41b0bd4b9b11a25fbd3636b3130b4
Mehmet Ali CAFER <mali.caferx@gmail.com>
2023-10-09 22:23:56 +0900

    Add camera controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region SerializedFields
+    [SerializeField] private Transform playerTransform;
+
+    [SerializeField] 
+    private Vector3 posOffset = new Vector3(0f, 1.5f, -10f);
+
+    [SerializeField] 
+    private float smooth = 0.25f;
+
+    private Vector3 velocity = Vector3.zero;
+    #endregion
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = playerTransform.position + posOffset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smooth);
+    }
+}
